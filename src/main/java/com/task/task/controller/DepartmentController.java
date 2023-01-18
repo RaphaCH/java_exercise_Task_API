dd6f4e76910b92c976bf1668385961b023b0ca9b
raphael <raphaelhaasper@gmail.com>
2023-01-18 13:42:49 +0100

    Applies Employee Service and Controller logic to Department counterparts

--- src/main/java/com/task/task/controller/DepartmentController.java
+++ src/main/java/com/task/task/controller/DepartmentController.java
@@ -1,18 +1,23 @@
 package com.task.task.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.task.task.model.Response;
+import com.task.task.model.CustomException;
+import com.task.task.model.Department;
+import com.task.task.model.ErrorMessage;
 import com.task.task.service.DepartmentServices;
 
 import jakarta.validation.Valid;
-
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080")
@@ -22,17 +27,30 @@ public class DepartmentController {
     @Autowired
     private DepartmentServices departmentServices;
 
-    
+    @GetMapping()
+    public ResponseEntity<?> getAllDepartments() {
+        try {
+            List<Department> response = departmentServices.retrieveAllDepartments();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (CustomException e) {
+            ErrorMessage errorMessage = new ErrorMessage(e.getStatusCode(), e.getErrorMessage(), e.getSubcode(),
+                    e.getDetails());
+            HttpStatus httpStatus = HttpStatus.valueOf(Integer.parseInt(errorMessage.getStatusCode()));
+            return new ResponseEntity<>(errorMessage, httpStatus);
+        }
+    }
 
-    // @GetMapping()
-    // public ResponseEntity<Response> getAllDepartments() {
-    //     // Response response = departmentServices.retrieveAllDepartments();
-    //     // return new ResponseEntity<Response>(response, response.getStatus());
-    // }
+    @PostMapping()
+    public ResponseEntity<?> createDepartment(@RequestBody @Valid Department department) {
+        try {
+            Department response = departmentServices.createNewDeparment(department);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (CustomException e) {
+            ErrorMessage errorMessage = new ErrorMessage(e.getStatusCode(), e.getErrorMessage(), e.getSubcode(),
+                    e.getDetails());
+            HttpStatus httpStatus = HttpStatus.valueOf(Integer.parseInt(errorMessage.getStatusCode()));
+            return new ResponseEntity<>(errorMessage, httpStatus);
+        }
 
-    // @PostMapping()
-    // public ResponseEntity<Response> createDepartment(@org.springframework.web.bind.annotation.RequestBody @Valid DepartmentDto department ) {
-    //     // Response response = departmentServices.createNewDeparment(department);
-    //     // return new ResponseEntity<Response>(response, response.getStatus());
-    // }
+    }
 }
--- src/main/java/com/task/task/service/DepartmentServices.java
+++ src/main/java/com/task/task/service/DepartmentServices.java
@@ -1,27 +1,54 @@
 package com.task.task.service;
 
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+import com.task.task.model.CustomException;
 import com.task.task.model.Department;
-
+import com.task.task.restTemplate.RestTemplate;
 
 @Service
 public class DepartmentServices {
-    
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     private static final Logger log = LoggerFactory.getLogger(DepartmentServices.class);
 
-    
-    public void createNewDeparment(Department departmentDto) {
-       
+    public List<Department> retrieveAllDepartments() throws CustomException {
+        try {
+            List<Department> departments = restTemplate.getAll("http://localhost:8080/departments", Department.class)
+                    .block();
+            return departments;
+        } catch (WebClientResponseException e) {
+            CustomException customException = e.getResponseBodyAs(CustomException.class);
+            log.error("Error inside catch statment {}", customException);
+            if (customException != null) {
+                throw customException;
+            }
+            return null;
+        }
     }
 
-    public void retrieveAllDepartments() {
-      
+    public Department createNewDeparment(Department departmentDto) throws CustomException {
+        try {
+            HttpEntity<Department> request = new HttpEntity<>(departmentDto);
+            Department response = restTemplate.post("http://localhost:8080/departments", request, Department.class).block();
+            return response;
+        } catch (WebClientResponseException e) {
+            CustomException customException = e.getResponseBodyAs(CustomException.class);
+            log.error("Error inside catch statment {}", customException);
+            if (customException != null) {
+                throw customException;
+            }
+            return null;
+        }
     }
 
-
-    
 }
